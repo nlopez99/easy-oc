3ac91eb6cd2151221f57b33a46a98cfcad1c6d21
Nino Lopez <nino.lopez@spathesystems.com>
2021-05-18 19:32:19 -0400

    Cleanup index file for consistency

--- index.js
+++ index.js
@@ -4,34 +4,22 @@ const prompt = require('prompt-sync')();
 const args = process.argv;
 const command = args[2];
 
-if (args.includes('--only-secrets') || args.includes('--os')) {
-    logInfo('Running Secret Generation...');
-    deploySecrets()
-      .then((res) => closeTool(0))
-      .catch((err) => {
-        console.log(err);
-        closeTool(1);
-      });
+const run = async () => {
+  try {
+    const username = prompt('OCP Username: ');
+    const password = prompt('OCP Password: ');
+    console.clear();
+
+    await logIntoOCP(username, password);
+    console.clear();
+
+    const ocpPods = await getOCPPods();
+    await portForwardPod(databasePod, 55432, 5432);
+    console.log(ocpPods)
+
+  } catch (err) {
+    console.log('Handling: ', err);
   }
-  
-
-
-async function run() {
-    try {
-        const username = prompt('OCP Username: ');
-        const password = prompt('OCP Password: ');
-        console.clear();
-
-        await logIntoOCP(username, password);
-        console.clear();
-
-        const ocpPods = await getOCPPods();
-        await portForwardPod(databasePod, 55432, 5432);
-        console.log(ocpPods)
-
-    } catch (err) {
-        console.log('Handling: ', err);
-    }
 }
 run();
 
