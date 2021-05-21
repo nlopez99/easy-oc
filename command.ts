792b41649c7318b4a2890c80309675a6f415d37c
Nino Lopez <nino.lopez@spathesystems.com>
2021-05-21 13:51:39 -0400

    Convert existing project to TS

---
+++ command.ts
@@ -0,0 +1,20 @@
+import { spawn } from 'child_process';
+
+
+export const runCommand = (command: string, args: string[]): Promise<string> => {
+    return new Promise<string>((resolve, reject) => {
+        const commandRunning = spawn(command, args);
+        let stdoutFeed: string = '';
+        commandRunning.stdout.on('data', (data: Buffer) => {
+            stdoutFeed += data.toString();
+        });
+        commandRunning.on('error', (data: Buffer) => {
+            reject(data.toString());
+
+        });
+        commandRunning.on('exit', (exitCode: number) => {
+            if (exitCode === 0) resolve(stdoutFeed);
+            else console.log(exitCode);
+        });
+    });
+};
